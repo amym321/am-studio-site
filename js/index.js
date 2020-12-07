26cf5871979a2cd889607e3d8f3addfd199771d4
amym321 <66137093+amym321@users.noreply.github.com>
2020-12-07 11:25:58 -0600

    smaller images with parallax

--- js/index.js
+++ js/index.js
@@ -78,17 +78,17 @@ view.setUpEventListeners();
 
 
 // parallax layers
-// window.addEventListener("scroll", function () {
-//     const topDistance = this.pageYOffset;
-//     const layers = document.querySelectorAll("[data-type='parallax']");
-//     layers.forEach(function (layer) {
-//         const depth = layer.getAttribute('data-depth');
-//         const movement = -(topDistance * depth);
-//         const translate3d = `translate3d(0, ${movement}px, 0)`;
-//         layer.style['-webkit-transform'] = translate3d;
-//         layer.style['-moz-transform'] = translate3d;
-//         layer.style['-ms-transform'] = translate3d;
-//         layer.style['-o-transform'] = translate3d;
-//         layer.style.transform = translate3d;
-//     });
-// });
+window.addEventListener("scroll", function () {
+    const topDistance = this.pageYOffset;
+    const layers = document.querySelectorAll("[data-type='parallax']");
+    layers.forEach(function (layer) {
+        const depth = layer.getAttribute('data-depth');
+        const movement = -(topDistance * depth);
+        const translate3d = `translate3d(0, ${movement}px, 0)`;
+        layer.style['-webkit-transform'] = translate3d;
+        layer.style['-moz-transform'] = translate3d;
+        layer.style['-ms-transform'] = translate3d;
+        layer.style['-o-transform'] = translate3d;
+        layer.style.transform = translate3d;
+    });
+});
